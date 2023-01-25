b7de35f4dfa416602989bba8450ea338eaffd6c2
obaid147 <obaidfayazwani@gmail.com>
2023-01-25 18:37:28 +0530

    Writing base test cases

--- build.sbt
+++ build.sbt
@@ -9,11 +9,13 @@ libraryDependencies ++= Seq(
   "org.slf4j" % "slf4j-nop" % "2.0.5",
   "com.typesafe.akka" %% "akka-stream" % "2.7.0",
   "com.typesafe.akka" %% "akka-http" % "10.4.0",
+  "com.typesafe.akka" %% "akka-http-spray-json" % "10.4.0",
   "org.json4s" %% "json4s-jackson" % "4.0.6","joda-time" % "joda-time" % "2.12.2",
   "org.postgresql" % "postgresql" % "42.5.1",
   "io.spray" %%  "spray-json" % "1.3.6",
-  "org.scalatest" %% "scalatest" % "3.1.1" % Test,
-  "com.typesafe.akka" %% "akka-http-testkit" % "10.4.0" % Test
+  "org.scalatest" %% "scalatest" % "3.1.3" % Test,
+  "com.typesafe.akka" %% "akka-http-testkit" % "10.4.0" % Test,
+  "com.typesafe.akka" %% "akka-stream-testkit" % "2.7.0"
   /*"com.typesafe.akka" %% "akka-http-core" % "1.0",
   //"com.typesafe.akka" %% "akka-http" % "1.0",
   */
--- src/test/scala/EmployeeRouteSpec.scala
+++ src/test/scala/EmployeeRouteSpec.scala
@@ -1,10 +1,69 @@
-import Controllers.EmployeeController
+import akka.http.scaladsl.model.{HttpResponse, RequestEntity, StatusCodes}
 import akka.http.scaladsl.testkit.ScalatestRouteTest
-import org.scalatest.matchers.should.Matchers
-import org.scalatest.WordSpec
-import EmployeeController.insertEmployeeController
+import org.scalatest.{Matchers, WordSpec}
+import spray.json._
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import akka.http.scaladsl.marshalling.Marshal
+import org.scalatest.concurrent.ScalaFutures.convertScalaFuture
 
-class EmployeeRouteSpec extends WordSpec with Matchers with ScalatestRouteTest{
+case class Employee(uuid: String, firstName: String, age: Long)
 
-  val route = new EmployeeRouteSpec()
+class EmployeeRouteSpec extends WordSpec with Matchers with ScalatestRouteTest {
+  import EmployeeRouteSpec._
+
+  "An employee backend" should {
+    "return all employees data" in {
+      Get("/company/employee") ~> myRoute ~> check {
+        status should be (StatusCodes.OK)
+
+        entityAs[Seq[Employee]] shouldBe myEmployee
+      }
+    }
+
+    "be able to add an employee" in {
+      val newEmployee = Employee(uuid = "3", firstName = "Bob", age = 35L)
+      val jsonRequest = Marshal(newEmployee).to[RequestEntity].futureValue
+      Post("/company/employee", jsonRequest) ~> myRoute ~> check {
+        status should be(StatusCodes.Created)
+        val response = entityAs[Employee]
+        response shouldBe newEmployee
+      }
+    }
+  }
 }
+
+
+trait EMPJsonProtocol extends DefaultJsonProtocol {
+  implicit val bookFormat: RootJsonFormat[Employee] = jsonFormat3(Employee)
+}
+
+import akka.http.scaladsl.server.Directives._
+
+object EmployeeRouteSpec extends EMPJsonProtocol with SprayJsonSupport {
+  var myEmployee = Seq(
+    Employee(
+      uuid = "1",
+      firstName = "John",
+      age = 24L
+    ),
+    Employee(
+      uuid = "2",
+      firstName = "Jane",
+      age = 30L
+    )
+  )
+
+  val myRoute = pathPrefix("company" / "employee") {
+    (pathEndOrSingleSlash & get) {
+      complete(myEmployee)
+    } ~ post {
+      entity(as[Employee]) { newEmployee =>
+        complete {
+          myEmployee = myEmployee :+ newEmployee
+          StatusCodes.Created -> newEmployee
+        }
+      }
+    }
+  }
+}
+
