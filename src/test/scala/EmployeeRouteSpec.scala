dae73d78de3df6850ba01352fba8d2f76405ece9
obaid147 <obaidfayazwani@gmail.com>
2023-01-25 19:07:49 +0530

    completed base tests

--- src/test/scala/EmployeeRouteSpec.scala
+++ src/test/scala/EmployeeRouteSpec.scala
@@ -1,9 +1,11 @@
-import akka.http.scaladsl.model.{HttpResponse, RequestEntity, StatusCodes}
+import akka.http.scaladsl.model.{RequestEntity, StatusCodes}
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import org.scalatest.{Matchers, WordSpec}
 import spray.json._
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.marshalling.Marshal
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
 import org.scalatest.concurrent.ScalaFutures.convertScalaFuture
 
 case class Employee(uuid: String, firstName: String, age: Long)
@@ -20,8 +22,8 @@ class EmployeeRouteSpec extends WordSpec with Matchers with ScalatestRouteTest {
       }
     }
 
-    "be able to add an employee" in {
-      val newEmployee = Employee(uuid = "3", firstName = "Bob", age = 35L)
+    "add an employee into database" in {
+      val newEmployee = Employee(uuid = "4", firstName = "Bob", age = 35L)
       val jsonRequest = Marshal(newEmployee).to[RequestEntity].futureValue
       Post("/company/employee", jsonRequest) ~> myRoute ~> check {
         status should be(StatusCodes.Created)
@@ -29,18 +31,32 @@ class EmployeeRouteSpec extends WordSpec with Matchers with ScalatestRouteTest {
         response shouldBe newEmployee
       }
     }
-  }
-}
 
+    "return an employee by id" in {
+      Get("/company/employee/1") ~> myRoute ~> check {
+        entityAs[Employee] shouldBe myEmployee.head
+        responseAs[Option[Employee]] shouldBe Some(Employee("1", "John",24L))
+      }
+    }
+
+    "return employees with same first name" in {
+      Get("/company/employee/firstName/John") ~> myRoute ~> check {
+        status should be(StatusCodes.OK)
+        entityAs[Seq[Employee]] shouldBe myEmployee.filter(_.firstName == "John")
+      }
+    }
+
+  }
+
+}
 
 trait EMPJsonProtocol extends DefaultJsonProtocol {
   implicit val bookFormat: RootJsonFormat[Employee] = jsonFormat3(Employee)
 }
 
-import akka.http.scaladsl.server.Directives._
 
 object EmployeeRouteSpec extends EMPJsonProtocol with SprayJsonSupport {
-  var myEmployee = Seq(
+  var myEmployee: Seq[Employee] = Seq(
     Employee(
       uuid = "1",
       firstName = "John",
@@ -48,12 +64,17 @@ object EmployeeRouteSpec extends EMPJsonProtocol with SprayJsonSupport {
     ),
     Employee(
       uuid = "2",
-      firstName = "Jane",
+      firstName = "Doe",
       age = 30L
+    ),
+    Employee(
+      uuid = "3",
+      firstName = "John",
+      age = 10L
     )
   )
 
-  val myRoute = pathPrefix("company" / "employee") {
+  val myRoute: Route = pathPrefix("company" / "employee") {
     (pathEndOrSingleSlash & get) {
       complete(myEmployee)
     } ~ post {
@@ -63,7 +84,15 @@ object EmployeeRouteSpec extends EMPJsonProtocol with SprayJsonSupport {
           StatusCodes.Created -> newEmployee
         }
       }
+    } ~ get {
+      path("firstName" / Segment) { fName =>
+        complete(myEmployee.filter(_.firstName == fName))
+      }
+    } ~ get {
+      (path(IntNumber) | parameter("uuid".as[Int])) { uuid =>
+        complete(myEmployee.find(_.uuid == uuid.toString))
+      }
     }
   }
-}
 
+}
